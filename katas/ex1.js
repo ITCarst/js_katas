d71e7a9dea46b15dbfad9aca207b06292834990a
IonutC <icarst@carst.me>
2015-09-09 21:55:07 +0200

    fixed a kata

--- katas/ex1.js
+++ katas/ex1.js
@@ -1,14 +1,7 @@
 define(function () {
 
     function filterWords (phrase) {
-        //phrase = phrase.toLowerCase(); 
-
-
-        console.log(phrase.replace(/(bad|mean|ugly|horrible|hideous)/,'awesome'));
-
-
-        return phrase.replace(/(bad|mean|ugly|horrible|hideous)/,'awesome');
-
+        return phrase.replace(/(bad|mean|ugly|horrible|hideous)/i,'awesome');
     }
 
     return filterWords;
--- test/ex1.test.js
+++ test/ex1.test.js
@@ -2,12 +2,11 @@ define(function (require) {
 
     var ex = require("/base/katas/ex1.js");
 
-
     describe("Fix Timmys Regex", function () {
         
         it("Fixed Tests", function () {
             expect(ex("You're Bad! timmy!")).toEqual("You're awesome! timmy!");
-            expect(ex("You're MEAN timmy!")).toEqual("You're awesome! timmy!");
+            expect(ex("You're MEAN! timmy!")).toEqual("You're awesome! timmy!");
         });
     
     });
